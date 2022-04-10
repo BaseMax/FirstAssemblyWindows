d5bfb5fcf9d64a8a05479ff3738af254aa5610b9
Max Base <MaxBaseCode@gmail.com>
2022-04-10 11:25:50 +0430

    working on asm

--- HelloAssembly.asm
+++ HelloAssembly.asm
@@ -24,5 +24,70 @@ WinMain proto :DWORD, :DWORD, :DWORD, :DWORD
 
 ; Constants and Datra
 
+WindowsWidth   equ   640                 ; How big we'd like our main window
+WindowsHeight  equ   480
 
+.DATA
+
+ClassName      db   "MyWinClass", 0      ; The name of our Window class
+AppName        db   "Tiny App", 0        ; The name of our main window
+
+.DATA?
+
+hInstance     HINSTANCE ?                ; Instance handle (like the process id) of our application
+CommandLine   LPSTR     ?                ; Pointer to the command line text we were lanuched WindowsWidth
+
+;-------------------------------------
+.CODE
+;-------------------------------------
+
+MainEntry:
+    push NULL
+    call GetModuleHandle
+    mov hInstance, eax
+
+    call GetCommandLine
+    move CommandLine, eax
+
+    ; Call our WinMain and then wxit the process with whatever comes back from it
+
+    push SW_SHOWDEFAULT
+    lea   eax, CommandLine
+    push eax
+    push NULL
+    push hInstance
+    call WinMain
+
+    push eax
+    call ExitProcess
+
+
+;
+; WinMain - The traditional signature for the main entry point of a Windows programa
+
+WinMain proc hInst:HINSTANCE, hPrevInst:HINSTANCE, CmdLine:LPSTR, CmdShow:DWORD
+    LOCAL   wc:WNDCLASSEX            ; Create these vars on the stack, hence LOCAL
+    LOCAL   msg:MSG
+    LOCAL   hwnd:HWND
+
+
+    mov wc.cvSize, SIZEOF WNDCLASSEX           ; Fill in the values in the members of our windowclass
+    mov wc.style, CS_HREDRAW or CS_VREDRAW     ; Redraw if resized in either dimension
+    mov wc.lpfnWndProc, OFFSET WndProc         ; Our callback function to handle window messages tc
+    mov wc.cbClsExtra, 0                       ; No extra class data
+    mov wc.cbWndExtra, 0                       ; No extra window data
+
+    mov eax, hInstance
+    mov wc.hInstance, eax
+    mov wc.hbrBackground, COLOR_3DSHADOW+1
+    mov wc.lpszMenuName, NULL                  ; No app menu
+    mov wc.lpszClassName, OFFSET ClassName     ; The window's class name
+
+    push IDI_APPLICATION                       ; Use the default application icon
+    push NULL
+    call LoadIcon
+    mov wc.hIcon, eax
+    mov wc.hIconSm, eax
+
+    
 
